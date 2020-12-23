78efe8cc7d2737ebdb57f2ee2cb6c3ea816da264
Vladislav Kosev <vladislav.kosev@clevertogether.com>
2020-12-23 22:30:45 +0200

    Fix WorkspaceIndex not reindexing when document changes.

--- tests/LanguageServerWorseReflection/Unit/Workspace/WorkspaceIndexTest.php
+++ tests/LanguageServerWorseReflection/Unit/Workspace/WorkspaceIndexTest.php
@@ -23,8 +23,8 @@ class WorkspaceIndexTest extends TestCase
 
     public function testIndexesClassesAndReturnsTextDocuments(): void
     {
-        $document1 = TextDocumentBuilder::create('<?php namespace Test {class Foobar {}}')->build();
-        $document2 = TextDocumentBuilder::create('<?php class Barfoo {}')->build();
+        $document1 = TextDocumentBuilder::create('<?php namespace Test {class Foobar {}}')->uri('/test/Foobar')->build();
+        $document2 = TextDocumentBuilder::create('<?php class Barfoo {}')->uri('/test/Barfoo')->build();
 
         $this->index->index($document1);
         $this->index->index($document2);
@@ -36,8 +36,8 @@ class WorkspaceIndexTest extends TestCase
 
     public function testIndexesFunctionsAndReturnsTextDocuments(): void
     {
-        $document1 = TextDocumentBuilder::create('<?php namespace Test {function barbar() {}}')->build();
-        $document2 = TextDocumentBuilder::create('<?php function barfoo{}')->build();
+        $document1 = TextDocumentBuilder::create('<?php namespace Test {function barbar() {}}')->uri('/Test/funcs')->build();
+        $document2 = TextDocumentBuilder::create('<?php function barfoo{}')->uri('/globals')->build();
 
         $this->index->index($document1);
         $this->index->index($document2);
@@ -57,6 +57,26 @@ class WorkspaceIndexTest extends TestCase
         $this->index->update($document1->uri(), $updatedText);
 
         self::assertSame($updatedText, $this->index->documentForName(Name::fromString('Test\Foobar'))->__toString());
+    }
+
+    public function testUpdateExistingEmptyDocument(): void
+    {
+        $document1 = TextDocumentBuilder::create('<?php')->uri('/test/foobar')->build();
+        $this->index->index($document1);
+
+        $updatedText = '<?php namespace Test {class Foobar {} function foofunc{}}';
+        $this->index->update($document1->uri(), $updatedText);
+
+        self::assertSame($updatedText, $this->index->documentForName(Name::fromString('Test\Foobar'))->__toString());
+        self::assertSame($updatedText, $this->index->documentForName(Name::fromString('Test\foofunc'))->__toString());
+
+        $updatedText = '<?php namespace Test2 {class Foobar {} function foofunc{}}';
+        $this->index->update($document1->uri(), $updatedText);
+
+        self::assertSame($updatedText, $this->index->documentForName(Name::fromString('Test2\Foobar'))->__toString());
+        self::assertSame($updatedText, $this->index->documentForName(Name::fromString('Test2\foofunc'))->__toString());
+        self::assertNull($this->index->documentForName(Name::fromString('Test\Foobar')));
+        self::assertNull($this->index->documentForName(Name::fromString('Test\foofunc')));
     }
 
     public function testRemovesTextDocument(): void
