4f319a7e8556f3fea8bfc8907f178618e4195d6f
Vladislav Kosev <vladislav.kosev@clevertogether.com>
2020-12-25 00:24:33 +0200

    Initial rename extension

--- lib/LanguageServerRename/Handler/RenameHandler.php
+++ lib/LanguageServerRename/Handler/RenameHandler.php
@@ -3,68 +3,38 @@
 namespace Phpactor\Extension\LanguageServerRename\Handler;
 
 use Amp\Promise;
-use Microsoft\PhpParser\Parser;
-use Phpactor\Extension\LanguageServerRename\Model\Renamer;
 use Phpactor\LanguageServerProtocol\PrepareRenameParams;
 use Phpactor\LanguageServerProtocol\PrepareRenameRequest;
 use Phpactor\LanguageServerProtocol\RenameOptions;
-use Phpactor\LanguageServerProtocol\RenameParams;
 use Phpactor\LanguageServerProtocol\ServerCapabilities;
 use Phpactor\LanguageServer\Core\Handler\CanRegisterCapabilities;
 use Phpactor\LanguageServer\Core\Handler\Handler;
-use Phpactor\LanguageServer\Core\Workspace\Workspace;
-use Phpactor\LanguageServerProtocol\RenameRequest;
 
 class RenameHandler implements Handler, CanRegisterCapabilities
 {
-    /**
-     * @var Workspace
-     */
-    private $workspace;
-    /**
-     * @var Parser
-     */
-    private $parser;
-    /**
-     * @var Renamer
-     */
-    private $renamer;
-
-
-    public function __construct(Workspace $workspace, Parser $parser, Renamer $renamer)
-    {
-        $this->workspace = $workspace;
-        $this->parser = $parser;
-        $this->renamer = $renamer;
-    }
     /**
      * {@inheritDoc}
      */
     public function methods(): array
     {
+        dump("Registering methods");
         return [
             PrepareRenameRequest::METHOD => 'prepareRename',
-            RenameRequest::METHOD => 'rename',
         ];
-    }
-
-    public function rename(RenameParams $params): Promise
-    {
-        return \Amp\call(function () use ($params) {
-            return $this->renamer->rename($this->workspace->get($params->textDocument->uri), $params->position, $params->newName);
-        });
     }
     
     public function prepareRename(PrepareRenameParams $params): Promise
     {
         // https://microsoft.github.io/language-server-protocol/specification#textDocument_prepareRename
         return \Amp\call(function () use ($params) {
-            return $this->renamer->prepareRename($this->workspace->get($params->textDocument->uri), $params->position);
+            $position = $params->position;
+            return null;
         });
     }
 
     public function registerCapabiltiies(ServerCapabilities $capabilities): void
     {
+        dump("Registering capabilities");
         $capabilities->renameProvider = new RenameOptions(true);
     }
 }
--- lib/LanguageServerRename/LanguageServerRenameExtension.php
+++ lib/LanguageServerRename/LanguageServerRenameExtension.php
@@ -2,19 +2,12 @@
 
 namespace Phpactor\Extension\LanguageServerRename;
 
-use Microsoft\PhpParser\Parser;
-use Phpactor\CodeTransform\Domain\Refactor\RenameVariable;
 use Phpactor\Container\Container;
 use Phpactor\Container\ContainerBuilder;
 use Phpactor\Container\Extension;
 use Phpactor\Extension\LanguageServerRename\Handler\RenameHandler;
-use Phpactor\Extension\LanguageServerRename\Model\NodeUtils;
-use Phpactor\Extension\LanguageServerRename\Model\Renamer;
 use Phpactor\Extension\LanguageServer\LanguageServerExtension;
-use Phpactor\Extension\ReferenceFinder\ReferenceFinderExtension;
-use Phpactor\LanguageServer\Core\Server\ClientApi;
 use Phpactor\MapResolver\Resolver;
-use Phpactor\ReferenceFinder\ReferenceFinder;
 
 class LanguageServerRenameExtension implements Extension
 {
@@ -23,19 +16,12 @@ class LanguageServerRenameExtension implements Extension
      */
     public function load(ContainerBuilder $container)
     {
+        dump("Loading extension");
         $container->register(RenameHandler::class, function (Container $container) {
             return new RenameHandler(
-                $container->get(LanguageServerExtension::SERVICE_SESSION_WORKSPACE),
-                new Parser(),
-                new Renamer(
-                    $container->get(LanguageServerExtension::SERVICE_SESSION_WORKSPACE),
-                    new Parser(),
-                    $container->get(ReferenceFinder::class),
-                    $container->get(ReferenceFinderExtension::SERVICE_DEFINITION_LOCATOR),
-                    $container->get(ClientApi::class),
-                    $container->get(RenameVariable::class),
-                    new NodeUtils(),
-                )
+                // $container->get(LanguageServerExtension::SERVICE_SESSION_WORKSPACE),
+                // $container->get(ReferenceFinderExtension::SERVICE_DEFINITION_LOCATOR),
+                // $container->get(LocationConverter::class)
             );
         }, [ LanguageServerExtension::TAG_METHOD_HANDLER => [] ]);
     }
