f554dcac182854655b8a8d8cbe52dc441956792a
Vladislav Kosev <vladislav.kosev@outlook.com>
2021-01-17 10:31:34 +0200

    Variable renamer

--- lib/LanguageServerRename/Model/Renamer/VariableRenamer.php
+++ lib/LanguageServerRename/Model/Renamer/VariableRenamer.php
@@ -3,6 +3,7 @@
 namespace Phpactor\Extension\LanguageServerRename\Model\Renamer;
 
 use Generator;
+use Microsoft\PhpParser\Node\Expression\Variable;
 use Phpactor\Extension\LanguageServerRename\Model\Renamer2;
 use Phpactor\TextDocument\ByteOffset;
 use Phpactor\TextDocument\ByteOffsetRange;
@@ -12,6 +13,8 @@ class VariableRenamer implements Renamer2
 {
     public function prepareRename(TextDocument $textDocument, ByteOffset $offset): ?ByteOffsetRange
     {
+        // if ($node instanceof Variable) {
+        // }
         return null;
     }
 
--- tests/LanguageServerRename/Unit/VariableRenamerTest.php
+++ tests/LanguageServerRename/Unit/VariableRenamerTest.php
@@ -2,12 +2,56 @@
 
 namespace Phpactor\Extension\LanguageServerRename\Tests\Unit;
 
+use Generator;
 use PHPUnit\Framework\TestCase;
+use Phpactor\Extension\LanguageServerBridge\Converter\PositionConverter;
+use Phpactor\Extension\LanguageServerRename\Model\Renamer\VariableRenamer;
+use Phpactor\Extension\LanguageServerRename\Tests\OffsetExtractor;
+use Phpactor\TextDocument\ByteOffset;
+use Phpactor\TextDocument\ByteOffsetRange;
+use Phpactor\TextDocument\TextDocumentBuilder;
 
 class VariableRenamerTest extends TestCase
 {
-    public function testTrue(): void
+    /** @dataProvider providePrepare */
+    public function testPrepare(string $source): void
     {
-        $this->assertTrue(true);
+        $offsetExtractor = new OffsetExtractor();
+        $offsetExtractor->registerPoint('selection', "<>", function(int $offset) { return ByteOffset::fromInt($offset); });
+        $offsetExtractor->registerRange(
+            "expectedRange", 
+            "{{", "}}", 
+            function (int $start, int $end) {
+                return new ByteOffsetRange(
+                    ByteOffset::fromInt($start),
+                    ByteOffset::fromInt($end),
+                );
+            }
+        );
+
+        [ 
+            'selection' => $selection, 
+            'expectedRange' => $expectedRange, 
+            'newSource' => $newSource 
+        ] = $offsetExtractor->parse($source);
+        
+        $document = TextDocumentBuilder::create($source)
+            ->uri("file:///test/testDoc")
+            ->build();
+        
+        $variableRenamer = new VariableRenamer();
+        $actualRange = $variableRenamer->prepareRename($document, $selection);
+        $this->assertEquals($expectedRange, $actualRange);
+    }
+
+    public function providePrepare(): Generator
+    {
+        yield 'Rename variable' => [
+            '<?php ${{va<>r1}} = 5;'
+        ];
+
+        // yield 'Rename variable 2' => [
+        //     '<?php class Class1 { public function method1(){ $${{va<>r1}} = 5; } }'
+        // ];
     }
 }
