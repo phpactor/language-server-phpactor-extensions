a2fc6db20fde6bdf32716ff3e148fc63c3b60ea6
Daniel Leech <dan.t.leech@gmail.com>
2021-02-08 21:01:26 +0000

    Added memoer renamer

---
+++ lib/LanguageServerRename/Model/Renamer/InMemoryRenamer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Phpactor\Extension\LanguageServerRename\Model\Renamer;
+
+use Generator;
+use Phpactor\Extension\LanguageServerRename\Model\Renamer;
+use Phpactor\TextDocument\ByteOffset;
+use Phpactor\TextDocument\ByteOffsetRange;
+use Phpactor\TextDocument\TextDocument;
+use Phpactor\TextDocument\Util\WordAtOffset;
+
+class InMemoryRenamer implements Renamer
+{
+    /**
+     * @var ByteOffsetRange
+     */
+    private $range;
+    /**
+     * @var array
+     */
+    private $results;
+
+    public function __construct(?ByteOffsetRange $range, array $results)
+    {
+        $this->results = $results;
+        $this->range = $range;
+    }
+
+    public function getRenameRange(TextDocument $textDocument, ByteOffset $offset): ?ByteOffsetRange
+    {
+         return $this->range;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rename(TextDocument $textDocument, ByteOffset $offset, string $newName): Generator
+    {
+        yield from $this->results;
+    }
+}
