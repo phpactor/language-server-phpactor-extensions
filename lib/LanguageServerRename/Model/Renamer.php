314fb057c2f615d237da57e9530bacc157cf1de1
Vladislav Kosev <vladislav.kosev@outlook.com>
2021-02-13 11:26:42 +0200

    Add rename infrastructure
    * The proper renamer structure
    
    * The proper renamer structure
    
    * The proper renamer structure
    
    * The proper renamer structure
    
    * Removed stub renamers
    
    * Moved chain renamer to correct namespace
    
    It is "pure" from dependencies - so it can live in model
    
    * Added memoer renamer
    
    * Refactored chain renamer test
    
    * Added test autoload rule
    
    This helps Phpactor detect what the class should be called.
    
    The weird autoloading is a side-effect of the inital migration to this
    bundle and the strange directory layout.
    
    * Renamed in extension
    
    * Moved test to correct namesace
    
    * Moved test files
    
    * Added integration test
    
    * CS fixes
    
    * Renmove setters
    
    * Use constant for example
    
    * Remove pointless array_merge
    
    * Add test for no renmaers
    
    * Renamed test
    
    * Renamed initialize
    
    * More renaming
    
    * Code style
    
    * Range converter
    
    Co-authored-by: Daniel Leech <dan.t.leech@gmail.com>

---
+++ lib/LanguageServerRename/Model/Renamer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Phpactor\Extension\LanguageServerRename\Model;
+
+use Generator;
+use Phpactor\TextDocument\ByteOffset;
+use Phpactor\TextDocument\ByteOffsetRange;
+use Phpactor\TextDocument\TextDocument;
+
+interface Renamer
+{
+    public function getRenameRange(TextDocument $textDocument, ByteOffset $offset): ?ByteOffsetRange;
+    /**
+     * @return Generator<RenameResult>
+     */
+    public function rename(TextDocument $textDocument, ByteOffset $offset, string $newName): Generator;
+}
---
+++ tests/LanguageServerRename/IntegrationTestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Phpactor\Extension\LanguageServerRename\Tests;
+
+use Phpactor\Extension\LanguageServerBridge\LanguageServerBridgeExtension;
+use Phpactor\Extension\LanguageServerRename\LanguageServerRenameExtension;
+use Phpactor\Extension\LanguageServerRename\Tests\Extension\TestExtension;
+use Phpactor\Extension\LanguageServer\LanguageServerExtension;
+use Phpactor\Extension\Logger\LoggingExtension;
+use Phpactor\FilePathResolverExtension\FilePathResolverExtension;
+use Phpactor\Container\PhpactorContainer;
+use Phpactor\Container\Container;
+use Phpactor\TestUtils\PHPUnit\TestCase;
+use Phpactor\TestUtils\Workspace;
+
+class IntegrationTestCase extends TestCase
+{
+    protected function workspace(): Workspace
+    {
+        return Workspace::create(__DIR__ . '/Workspace');
+    }
+
+    protected function container(array $config = []): Container
+    {
+        $container = PhpactorContainer::fromExtensions([
+            LanguageServerExtension::class,
+            TestExtension::class,
+            LanguageServerRenameExtension::class,
+            FilePathResolverExtension::class,
+            LanguageServerBridgeExtension::class,
+            LoggingExtension::class,
+        ], $config);
+
+        return $container;
+    }
+}
