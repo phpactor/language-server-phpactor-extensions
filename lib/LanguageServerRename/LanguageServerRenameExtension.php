b57154290c7f6d72d94208b759456fbc87279e3b
Daniel Leech <dan.t.leech@gmail.com>
2021-04-11 11:00:45 +0100

    Enable the renamer

--- lib/LanguageServerRename/LanguageServerRenameExtension.php
+++ lib/LanguageServerRename/LanguageServerRenameExtension.php
@@ -5,18 +5,24 @@ namespace Phpactor\Extension\LanguageServerRename;
 use Phpactor\Container\Container;
 use Phpactor\Container\ContainerBuilder;
 use Phpactor\Container\Extension;
+use Phpactor\Extension\LanguageServerRename\Adapter\Worse\RenameLocationsProvider;
+use Phpactor\Extension\LanguageServerRename\Adapter\Worse\VariableRenamer;
 use Phpactor\Extension\LanguageServerRename\Model\Renamer\ChainRenamer;
 use Phpactor\Extension\LanguageServerRename\Handler\RenameHandler;
 use Phpactor\Extension\LanguageServerRename\Model\Renamer;
 use Phpactor\Extension\LanguageServer\LanguageServerExtension;
+use Phpactor\Extension\ReferenceFinder\ReferenceFinderExtension;
+use Phpactor\Extension\WorseReflection\WorseReflectionExtension;
 use Phpactor\MapResolver\Resolver;
+use Phpactor\ReferenceFinder\DefinitionLocator;
 use Phpactor\TextDocument\TextDocumentLocator;
+use Phpactor\WorseReferenceFinder\TolerantVariableReferenceFinder;
 
 class LanguageServerRenameExtension implements Extension
 {
     public const TAG_RENAMER = 'language_server_rename.renamer';
-
     /**
+
      * {@inheritDoc}
      */
     public function load(ContainerBuilder $container): void
@@ -33,7 +39,11 @@ class LanguageServerRenameExtension implements Extension
                 $container->get(TextDocumentLocator::class),
                 $container->get(Renamer::class),
             );
-        }, [ LanguageServerExtension::TAG_METHOD_HANDLER => [] ]);
+        }, [
+            LanguageServerExtension::TAG_METHOD_HANDLER => []
+        ]);
+
+        $this->registerWorseRenamers($container);
     }
 
     /**
@@ -41,5 +51,25 @@ class LanguageServerRenameExtension implements Extension
      */
     public function configure(Resolver $schema): void
     {
+    }
+
+    private function registerWorseRenamers(ContainerBuilder $container)
+    {
+        $container->register(RenameLocationsProvider::class, function (Container $container) {
+            return new RenameLocationsProvider(
+                $container->get('worse_reference_finder.reference_finder.variable'),
+                $container->get(ReferenceFinderExtension::SERVICE_DEFINITION_LOCATOR)
+            );
+        });
+
+        $container->register(VariableRenamer::class, function (Container $container) {
+            return new VariableRenamer(
+                $container->get(RenameLocationsProvider::class),
+                $container->get(TextDocumentLocator::class),
+                $container->get('worse_reflection.tolerant_parser')
+            );
+        }, [
+            LanguageServerRenameExtension::TAG_RENAMER => []
+        ]);
     }
 }
