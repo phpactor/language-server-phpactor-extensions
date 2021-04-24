16a17f60f2eaeea868a561ac0f72f58371d34258
Daniel Leech <dan.t.leech@gmail.com>
2021-04-24 10:38:46 +0100

    Working test

--- lib/LanguageServerRename/Adapter/ReferenceFinder/ClassMover/FileRenamer.php
+++ lib/LanguageServerRename/Adapter/ReferenceFinder/ClassMover/FileRenamer.php
@@ -4,12 +4,16 @@ namespace Phpactor\Extension\LanguageServerRename\Adapter\ReferenceFinder\ClassM
 
 use Amp\Promise;
 use Phpactor\ClassFileConverter\ClassToFileConverter;
+use Phpactor\ClassFileConverter\Domain\ClassName;
 use Phpactor\ClassFileConverter\Domain\ClassToFile;
 use Phpactor\ClassFileConverter\Domain\FilePath;
 use Phpactor\ClassFileConverter\Domain\FileToClass;
 use Phpactor\ClassMover\ClassMover;
 use Phpactor\Extension\LanguageServerRename\Model\Exception\CouldNotRename;
 use Phpactor\Extension\LanguageServerRename\Model\FileRenamer as PhpactorFileRenamer;
+use Phpactor\Extension\LanguageServerRename\Model\LocatedTextEdit;
+use Phpactor\Extension\LanguageServerRename\Model\LocatedTextEdits;
+use Phpactor\Extension\LanguageServerRename\Model\LocatedTextEditsMap;
 use Phpactor\Indexer\IndexAgent;
 use Phpactor\Indexer\Model\QueryClient;
 use Phpactor\TextDocument\TextDocumentLocator;
@@ -65,17 +69,43 @@ class FileRenamer implements PhpactorFileRenamer
 
             $references = $this->client->class()->referencesTo($fromClass->__toString());
 
+            // rename class definition
+            $locatedEdits = $this->replaceDefinition($to, $fromClass, $toClass);
+
             $edits = TextEdits::none();
+            $seen = [];
             foreach ($references as $reference) {
+                if (isset($seen[$reference->location()->uri()->path()])) {
+                    continue;
+                }
+
+                $seen[$reference->location()->uri()->path()] = true;
+
                 $document = $this->locator->get($reference->location()->uri());
 
-                $edits = $edits->merge($this->mover->replaceReferences(
+                foreach ($this->mover->replaceReferences(
                     $this->mover->findReferences($document->__toString(), $fromClass),
                     $toClass
-                ));
+                ) as $edit) {
+                $locatedEdits[] = new LocatedTextEdit($reference->location()->uri(), $edit);
+                }
             }
 
-            return $edits;
+            return LocatedTextEditsMap::fromLocatedEdits($locatedEdits);
         });
+    }
+
+    private function replaceDefinition(TextDocumentUri $file, ClassName $fromClass, ClassName $toClass): array
+    {
+        $document = $this->locator->get($file);
+        $locatedEdits = [];
+        foreach ($this->mover->replaceReferences(
+            $this->mover->findReferences($document, $fromClass),
+            $toClass
+        ) as $edit) {
+            $locatedEdits[] = new LocatedTextEdit($file, $edit);
+        }
+
+        return $locatedEdits;
     }
 }
