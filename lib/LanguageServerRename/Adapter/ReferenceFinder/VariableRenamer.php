f9f0b6c05de1bbda7978d09b2846e8c82670194c
Vladislav Kosev <vladislav.kosev@outlook.com>
2021-05-22 14:18:56 +0300

    Detect variables for rename in use statements and catch clauses

--- lib/LanguageServerRename/Adapter/ReferenceFinder/VariableRenamer.php
+++ lib/LanguageServerRename/Adapter/ReferenceFinder/VariableRenamer.php
@@ -3,9 +3,11 @@
 namespace Phpactor\Extension\LanguageServerRename\Adapter\ReferenceFinder;
 
 use Microsoft\PhpParser\Node;
+use Microsoft\PhpParser\Node\CatchClause;
 use Microsoft\PhpParser\Node\Expression\Variable;
 use Microsoft\PhpParser\Node\Parameter;
 use Microsoft\PhpParser\Node\PropertyDeclaration;
+use Microsoft\PhpParser\Node\UseVariableName;
 use Phpactor\TextDocument\ByteOffsetRange;
 
 class VariableRenamer extends AbstractReferenceRenamer
@@ -19,7 +21,14 @@ class VariableRenamer extends AbstractReferenceRenamer
             return $this->offsetRangeFromToken($node->name, true);
         }
 
-        if ($node instanceof Parameter) {
+        if (
+            (
+                $node instanceof Parameter ||
+                $node instanceof UseVariableName ||
+                $node instanceof CatchClause
+            ) &&
+            $node->variableName !== null
+        ) {
             return $this->offsetRangeFromToken($node->variableName, true);
         }
 
