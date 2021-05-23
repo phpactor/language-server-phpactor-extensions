609d6ec5576507aa2430687d5d6d6294a1aa7995
Daniel Leech <dan.t.leech@gmail.com>
2021-05-23 22:07:05 +0100

    Removed test file

--- lib/test2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use PhpBench\Tests\Unit\Benchmark\Metadata\classes\Test;
-use Phpactor\ClassMover\FoundReferences;
-use http\Client\Request;
-
-new Request;
-new Test;
-new FoundReferences;
